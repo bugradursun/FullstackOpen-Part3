e68ef83637d46b70d1ce90aca235d4997ae152fd
bugradursun <72981010+bugradursun@users.noreply.github.com>
2024-08-13 13:29:24 +0300

    changed directory

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,45 +1,41 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React from "react";
-
+/* eslint-disable react/prop-types */ /* eslint-disable no-unused-vars */ import React from "react";
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return <p>total of {total} exercises</p>;
 };
-
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
-
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {" "}
+      {props.part} {props.exercises}{" "}
     </p>
   );
 };
-
 const Content = ({ parts }) => {
   return (
     <div>
+      {" "}
       {parts.map((part) => (
         <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
+      ))}{" "}
     </div>
   );
 };
 const Course = ({ courses }) => {
   return (
     <div>
+      {" "}
       {courses.map((course) => (
         <div key={course.id}>
-          <Header course={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
+          {" "}
+          <Header course={course.name} /> <Content parts={course.parts} />{" "}
+          <Total parts={course.parts} />{" "}
         </div>
-      ))}
+      ))}{" "}
     </div>
   );
 };
-
 export default Course;
--- part2/part2content/src/components/Note.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import React from "react";
-
-const Note = ({ note }) => {
-  return <li>{note.content}</li>;
-};
-
-export default Note;
