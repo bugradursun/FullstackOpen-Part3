ca7904ce1662a82e1613ea0fdc02064b3f738880
bugradursun <72981010+bugradursun@users.noreply.github.com>
2024-08-13 11:58:02 +0300

    refactored

---
+++ part2/part2content/src/components/Note.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const Note = ({ note }) => {
+  return <li>{note.content}</li>;
+};
+
+export default Note;
